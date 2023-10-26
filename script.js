d340603e424996d2366c2042287d40e8fbf921cb
MarVasilieva <marina_vasilieva@list.ru>
2023-10-26 12:06:10 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+"use sctrict";
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(typeof answer);
+const answers = [];
+// answers[0] = prompt("Как ваше иммя?", "");
+// answers[1] = prompt("Как ваше фамилия?", "");
+// answers[2] = prompt("Как вам лет ?", "");
+// document.write(answers);
+console.log(typeof answers);
